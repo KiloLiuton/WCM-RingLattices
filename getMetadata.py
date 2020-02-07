b2b6bf406aeef0a5d34adc4f6192ef8e741d2862
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2020-02-07 10:55:03 -0300

    fix default filename function not checking for existing versions and overwriting them

--- getMetadata.py
+++ getMetadata.py
@@ -3,8 +3,8 @@ keys = []
 def getMetadata(arq):
     with open(arq, "r") as f:
         metadata = f.readline()
-    metadata = [x.split('=') for x in metadata.split()]
-    print(metadata)
+    metadata = [x.split('=') for x in metadata.split(' ')]
+    return dict(metadata)
 
 if __name__ == "__main__":
     import sys
