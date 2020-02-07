957e978b873a1be8b2fa3a044a1bcac39e790913
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2020-02-07 10:55:16 -0300

    fix default filename function not checking for existing versions and overwriting them

--- logging.hpp
+++ logging.hpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <string>
 #include <time.h>
+#include <fstream>
 
 std::string defaultFilePath(int N, int K, const char* affix, const char* suffix)
 {
@@ -10,13 +11,18 @@ std::string defaultFilePath(int N, int K, const char* affix, const char* suffix)
     time_t tt;
     time(&tt);
     struct tm* ti = localtime(&tt);
-    strftime(date, 64, "%F-%R", ti);
+    strftime(date, 64, "%F-%b-%a", ti);
 
     int version = 0;
-    sprintf(filename, "%s-N-%06dK-%06d%s_v%d.dat", affix, N, K, suffix, version);
     sprintf(path, "data/%s/", date);
     std::string cmd = "mkdir -p " + std::string(path);
     system(cmd.c_str());
+
+    sprintf(filename, "%s-N-%06dK-%06d%s_v%d.dat", affix, N, K, suffix, version);
+    while (std::ifstream(std::string(path) + std::string(filename))) {
+        version++;
+        sprintf(filename, "%s-N-%06dK-%06d%s_v%d.dat", affix, N, K, suffix, version);
+    }
 
     return std::string(path) + std::string(filename);
 }
